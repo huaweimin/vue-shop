bc9adc287cfa7af57abe1a3c3c3e0ea4b2d38090
hwm <416553906@qq.com>
2020-05-19 23:20:53 +0800

    first commit

---
+++ client/src/pages/Cart.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="title">
+      <h1>{{msg}}</h1>
+    </div>
+    <product-item :products="cart"></product-item>
+  </div>
+</template>
+
+<script>
+import ProductItem from '@/components/products/ProductItem'
+
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: '购物车'
+    }
+  },
+  computed: {
+    cart () {
+      return this.$store.state.cart
+    }
+  },
+  components: {
+    'product-item': ProductItem
+  }
+}
+</script>
---
+++ client/src/pages/admin/Index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div class="admin-new">
+      <div class="container">
+        <h1>后台管理</h1>
+        <router-view/>
+      </div>
+    </div>
+  </div>
+</template>
